a5751f62a78db0bc7aecdbf1f9385b4e552c0dcf
Thomas Naunheim <thomas@naunheim.net>
2024-08-28 07:54:19 +0200

    Updated classification for Intune

--- Scripts/Export-EntraOpsClassificationDeviceManagementRoles.ps1
+++ Scripts/Export-EntraOpsClassificationDeviceManagementRoles.ps1
@@ -17,14 +17,14 @@ function Export-EntraOpsClassificationDeviceManagementRoles {
     )
 
     # Get EntraOps Classification
-    $Classification = Get-Content -Path ./EntraOps_Classification/Classification_Intune.json | ConvertFrom-Json -Depth 10
+    $Classification = Get-Content -Path ./EntraOps_Classification/Classification_DeviceManagement.json | ConvertFrom-Json -Depth 10
 
     # Single classifcation (highest tier level only)
     Write-Output "Query directory role templates for mapping ID to name and further details"
-    $DeviceManagementRoleDefinitions = (Invoke-EntraOpsMsGraphQuery -Uri "https://graph.microsoft.com/beta/roleManagement/deviceManagement/roleDefinitions") | select-object displayName, templateId, isBuiltin, isPrivileged, rolePermissions
+    $DeviceManagementRoleDefinitions = (Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/roleManagement/deviceManagement/roleDefinitions").value | select-object displayName, templateId, isBuiltin, isPrivileged, rolePermissions
 
     if ($IncludeCustomRoles -eq $False) {
-        $DeviceManagementRoleDefinitions = $DeviceManagementRoleDefinitions | where-object {$_.isBuiltin -eq "True"}
+        $DeviceManagementRoleDefinitions = $DeviceManagementRoleDefinitions | where-object { $_.isBuiltin -eq "True" }
     }
 
     $DeviceManagementRoles = $DeviceManagementRoleDefinitions | foreach-object {
@@ -32,8 +32,8 @@ function Export-EntraOpsClassificationDeviceManagementRoles {
         $DeviceRolePermissions = $_.RolePermissions.allowedResourceActions
         $ClassifiedDeviceRolePermissions = foreach ($RolePermission in $DeviceRolePermissions) {
             # Apply Classification
-            $DeviceMgmtRolePermissionTierLevelClassification = $Classification | where-object {$_.TierLevelDefinition.RoleDefinitionActions -contains $($RolePermission) -and $_.TierLevelDefinition.RoleAssignmentScopeName -eq $DefaultScope} | select-object EAMTierLevelName, EAMTierLevelTagValue
-            $DeviceMgmtRolePermissionServiceClassification = $Classification | select-object -ExpandProperty TierLevelDefinition | where-object {$_.RoleDefinitionActions -contains $($RolePermission) -and $_.RoleAssignmentScopeName -eq $DefaultScope} | select-object Service
+            $DeviceMgmtRolePermissionTierLevelClassification = $Classification | where-object { $_.TierLevelDefinition.RoleDefinitionActions -contains $($RolePermission) -and $_.TierLevelDefinition.RoleAssignmentScopeName -eq $DefaultScope } | select-object EAMTierLevelName, EAMTierLevelTagValue
+            $DeviceMgmtRolePermissionServiceClassification = $Classification | select-object -ExpandProperty TierLevelDefinition | where-object { $_.RoleDefinitionActions -contains $($RolePermission) -and $_.RoleAssignmentScopeName -eq $DefaultScope } | select-object Service
 
             if ($DeviceMgmtRolePermissionTierLevelClassification.Count -gt 1 -and $DeviceMgmtRolePermissionServiceClassification.Count -gt 1) {
                 Write-Warning "Multiple Tier Level Classification found for $($RolePermission)"
@@ -41,40 +41,40 @@ function Export-EntraOpsClassificationDeviceManagementRoles {
 
             if ($null -eq $DeviceMgmtRolePermissionTierLevelClassification) {
                 $DeviceMgmtRolePermissionTierLevelClassification = [PSCustomObject]@{
-                    "EAMTierLevelName"      = "Unclassified"
-                    "EAMTierLevelTagValue"  = "Unclassified"
+                    "EAMTierLevelName"     = "Unclassified"
+                    "EAMTierLevelTagValue" = "Unclassified"
                 }
             }
 
             if ($null -eq $DeviceMgmtRolePermissionServiceClassification) {
                 $DeviceMgmtRolePermissionServiceClassification = [PSCustomObject]@{
-                    "Service"             = "Unclassified"
+                    "Service" = "Unclassified"
                 }
             }
 
             [PSCustomObject]@{
-                "AuthorizedResourceAction"  = $RolePermission
-                "Category"                  = $DeviceMgmtRolePermissionServiceClassification.Service
-                "EAMTierLevelName"          = $DeviceMgmtRolePermissionTierLevelClassification.EAMTierLevelName
-                "EAMTierLevelTagValue"      = $DeviceMgmtRolePermissionTierLevelClassification.EAMTierLevelTagValue
+                "AuthorizedResourceAction" = $RolePermission
+                "Category"                 = $DeviceMgmtRolePermissionServiceClassification.Service
+                "EAMTierLevelName"         = $DeviceMgmtRolePermissionTierLevelClassification.EAMTierLevelName
+                "EAMTierLevelTagValue"     = $DeviceMgmtRolePermissionTierLevelClassification.EAMTierLevelTagValue
             }
         }
 
         if ($SingleClassification -eq $True) {
-            $RoleDefinitionClassification            = ($ClassifiedDeviceRolePermissions | select-object -ExcludeProperty AuthorizedResourceAction, Category -Unique | Sort-Object EAMTierLevelTagValue | select-object -First 1)
+            $RoleDefinitionClassification = ($ClassifiedDeviceRolePermissions | select-object -ExcludeProperty AuthorizedResourceAction, Category -Unique | Sort-Object EAMTierLevelTagValue | select-object -First 1)
         }
         else {
-            $FilteredRoleClassifications            = ($ClassifiedDeviceRolePermissions | select-object -ExcludeProperty AuthorizedResourceAction -Unique | Sort-Object EAMTierLevelTagValue )
-            $RoleDefinitionClassification           = [System.Collections.Generic.List[object]]::new()
+            $FilteredRoleClassifications = ($ClassifiedDeviceRolePermissions | select-object -ExcludeProperty AuthorizedResourceAction -Unique | Sort-Object EAMTierLevelTagValue )
+            $RoleDefinitionClassification = [System.Collections.Generic.List[object]]::new()
             $RoleDefinitionClassification.Add($FilteredRoleClassifications)
         }
 
         [PSCustomObject]@{
-            "RoleId"                = $_.templateId
-            "RoleName"              = $_.displayName
-            "isPrivileged"          = $_.isPrivileged
-            "RolePermissions"       = $ClassifiedDeviceRolePermissions
-            "Classification"        = $RoleDefinitionClassification
+            "RoleId"          = $_.templateId
+            "RoleName"        = $_.displayName
+            "isPrivileged"    = $_.isPrivileged
+            "RolePermissions" = $ClassifiedDeviceRolePermissions
+            "Classification"  = $RoleDefinitionClassification
         }
     }
 
